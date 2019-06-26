749a4cb384a1018a930d5fcbb511f4c6ad1d1d72
League Student <league@iMac-18.attlocal.net>
2019-06-25 19:54:42 -0700

    6/25/19
    module 1!

---
+++ src/Platypus.java
@@ -0,0 +1,12 @@
+
+public class Platypus {
+String name;
+Platypus(String name){
+this.name =name;
+}
+
+void sayHi(String name) {
+	
+	System.out.println("The platypus " + name + " is smarter than you average platypus.");
+}
+}
---
+++ src/PlatypusRunner.java
@@ -0,0 +1,10 @@
+
+
+public class PlatypusRunner {
+	public static void main(String[] args) {
+
+			Platypus William = new Platypus("William");
+			William.sayHi("John");
+		}
+
+}
